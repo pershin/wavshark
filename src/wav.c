ee511b97404441e90a00b58b85f7115c4a600edf
Sergey Pershin <sergey.pershin@hotmail.com>
2026-05-15 01:02:30 +0300

    Add wav_find_chunk()

--- src/wav.c
+++ src/wav.c
@@ -7,10 +7,13 @@
 
 struct wav_handle_struct {
   FILE *stream;
+  uint32_t size;
 };
 
 #define RIFF_MARKER 0x46464952 /* RIFF */
 #define WAVE_MARKER 0x45564157 /* WAVE */
+#define FMT_MARKER 0x20746d66  /* fmt  */
+#define DATA_MARKER 0x61746164 /* data */
 
 #pragma pack(push, 1)
 
@@ -21,6 +24,7 @@ typedef struct {
 
 #pragma pack(pop)
 
+static bool wav_find_chunk(FILE *stream, uint32_t id, wav_chunk *chunk);
 static bool wav_read_header(wav_handle *handle);
 
 wav_handle *wav_open(const char *filename) {
@@ -67,6 +71,26 @@ void wav_close(wav_handle *handle) {
   }
 }
 
+static bool wav_find_chunk(FILE *stream, uint32_t id, wav_chunk *chunk) {
+  /* Skip RIFF header. */
+  if (fseek(stream, sizeof(wav_chunk) + sizeof(uint32_t), SEEK_SET) != 0)
+    return false;
+
+  while (fread(chunk, sizeof(wav_chunk), 1, stream) == 1) {
+    if (0 == chunk->size)
+      return false;
+
+    if (id == chunk->id)
+      return true;
+
+    /* Skip chunk data (aligned to 2 bytes). */
+    if (fseek(stream, (chunk->size + 1) & ~1, SEEK_CUR) != 0)
+      return false;
+  }
+
+  return false;
+}
+
 static bool wav_read_header(wav_handle *handle) {
   wav_chunk chunk = {0};
   uint32_t type = 0;
@@ -82,6 +106,14 @@ static bool wav_read_header(wav_handle *handle) {
 
   if (WAVE_MARKER != type)
     return false;
+
+  if (wav_find_chunk(handle->stream, FMT_MARKER, &chunk) == false)
+    return false;
+
+  if (wav_find_chunk(handle->stream, DATA_MARKER, &chunk) == false)
+    return false;
+
+  handle->size = chunk.size;
 
   return true;
 }
