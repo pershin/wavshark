79c0f6e2bfe59d6c68cafeea4d6c82143240f746
Sergey Pershin <sergey.pershin@hotmail.com>
2026-05-14 23:17:37 +0300

    Add wav_read_header()

--- src/wav.c
+++ src/wav.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <errno.h>
 #include "wav.h"
@@ -7,6 +8,20 @@
 struct wav_handle_struct {
   FILE *stream;
 };
+
+#define RIFF_MARKER 0x46464952 /* RIFF */
+#define WAVE_MARKER 0x45564157 /* WAVE */
+
+#pragma pack(push, 1)
+
+typedef struct {
+  uint32_t id;
+  uint32_t size;
+} wav_chunk;
+
+#pragma pack(pop)
+
+static bool wav_read_header(wav_handle *handle);
 
 wav_handle *wav_open(const char *filename) {
   wav_handle *handle = NULL;
@@ -21,6 +36,12 @@ wav_handle *wav_open(const char *filename) {
   if (NULL == handle->stream) {
     fprintf(stderr, PACKAGE_NAME ": Cannot open file '%s': %s\n", filename,
             strerror(errno));
+    goto cleanup;
+  }
+
+  if (wav_read_header(handle) == false) {
+    fprintf(stderr, PACKAGE_NAME ": Failed to read WAV header from '%s'\n",
+            filename);
     goto cleanup;
   }
 
@@ -44,4 +65,23 @@ void wav_close(wav_handle *handle) {
 
     free(handle);
   }
+}
+
+static bool wav_read_header(wav_handle *handle) {
+  wav_chunk chunk = {0};
+  uint32_t type = 0;
+
+  if (fread(&chunk, sizeof(wav_chunk), 1, handle->stream) != 1)
+    return false;
+
+  if (RIFF_MARKER != chunk.id)
+    return false;
+
+  if (fread(&type, sizeof(uint32_t), 1, handle->stream) != 1)
+    return false;
+
+  if (WAVE_MARKER != type)
+    return false;
+
+  return true;
 }
